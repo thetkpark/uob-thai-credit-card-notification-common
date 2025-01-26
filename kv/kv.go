bd2d1501e5a505ba7b459a289b7fe74f9308014b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2025-01-26 15:40:19 +0700

    remove ttl

--- kv/kv.go
+++ kv/kv.go
@@ -1,8 +1,12 @@
 package kv
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type KV interface {
-	Add(ctx context.Context, k string) error
+	Add(ctx context.Context, k string, v string, ttl time.Duration) error
+	Get(ctx context.Context, k string) (string, error)
 	Exist(ctx context.Context, k string) (bool, error)
 }
